fd867435ced9a9c1946594998bac82d90df7d232
GVabal <gvabal@gmail.com>
2020-11-12 13:21:01 +0200

    :egg: Add basic structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginPageComponent} from './login-page/login-page.component';
+import {UserPageComponent} from './user-page/user-page.component';
+import {EmployeePageComponent} from './employee-page/employee-page.component';
+import {AdminPageComponent} from './admin-page/admin-page.component';
+import {HomePageComponent} from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent,
+  },
+  {
+    path: 'user',
+    component: UserPageComponent,
+  },
+  {
+    path: 'employee',
+    component: EmployeePageComponent,
+  },
+  {
+    path: 'admin',
+    component: AdminPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { AdminPageComponent } from './admin-page/admin-page.component';
+import { UserPageComponent } from './user-page/user-page.component';
+import { EmployeePageComponent } from './employee-page/employee-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    AdminPageComponent,
+    UserPageComponent,
+    EmployeePageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
