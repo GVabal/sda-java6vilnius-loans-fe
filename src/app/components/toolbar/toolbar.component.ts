8490b175a94817f08d24600363c32be4ea97be3a
GVabal <gvabal@gmail.com>
2020-12-05 10:04:01 +0200

    Show email while logged in

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../services/auth.service';
 import {Router} from '@angular/router';
+import {User} from '../../interfaces/models/User';
 
 @Component({
   selector: 'app-toolbar',
@@ -9,10 +10,19 @@ import {Router} from '@angular/router';
 })
 export class ToolbarComponent implements OnInit {
 
+  user: User = {email: '', role: ''};
+
   constructor(public authService: AuthService, private router: Router) {
   }
 
   ngOnInit(): void {
+    this.authService.auth.subscribe(authResponse => {
+      try {
+      this.user = authResponse.user;
+      } catch (error) {
+        // nothing
+      }
+    });
   }
 
   logout(): void {
